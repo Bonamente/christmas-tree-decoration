01e06aa8a883c31c116d56284a1cc06639de1194
Bonamente <56506035@protonmail.com>
2021-12-19 20:10:20 +0500

    feat: add renders

---
+++ christmas-task-part1/src/app/renders/page-render.ts
@@ -0,0 +1,14 @@
+import { IState } from '../app';
+import buildHeader from '../builders/header';
+import buildPageContent from '../builders/page-content';
+import buildFooter from '../builders/footer';
+
+const renderPage = (state: IState) => {
+  const header = buildHeader(state);
+  const pageContent = buildPageContent(state);
+  const footer = buildFooter();
+
+  document.body.append(header, pageContent, footer);
+};
+
+export default renderPage;
---
+++ christmas-task-part1/src/app/renders/render-searched-cards.ts
@@ -0,0 +1,22 @@
+import { IState } from './../app';
+import buildCard from '../builders/cards/card';
+
+const renderSearchedCards = (state: IState) => {
+  const headerControls = document.querySelector('.header__controls') as HTMLElement; 
+  const cardsList = document.querySelector('.cards__list') as HTMLElement;
+
+  const cardsData = Array.from(state.uiState.searchedToys).flat();
+  const cards = cardsData.map((item) => buildCard(state, item));  
+
+  if (cards.length === 0) {
+    headerControls.classList.add('header__controls--no-matches');
+    cardsList.innerHTML = '';
+    return;
+  }
+
+  headerControls.classList.remove('header__controls--no-matches');
+  cardsList.innerHTML = '';
+  cardsList.append(...cards);
+};
+
+export default renderSearchedCards;
