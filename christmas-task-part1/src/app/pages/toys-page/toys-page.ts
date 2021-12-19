fc66043b8594ffa4fe9fc906b51747e9b172dc66
Bonamente <56506035@protonmail.com>
2021-12-19 20:09:35 +0500

    feat: add toys page

---
+++ christmas-task-part1/src/app/pages/toys-page/toys-page.ts
@@ -0,0 +1,41 @@
+import { IState } from '../../app';
+import buildValueFilters from '../../builders/controls/value-filters/value-filters';
+import buildRangeFilters from '../../builders/controls/range-filters';
+import buildSortSection from '../../builders/controls/sort -section';
+import buildCards from '../../builders/cards/cards';
+
+const buildToysPageControls = (state: IState) => {
+  const controlsSection = document.createElement('section');
+  controlsSection.classList.add('controls');
+  controlsSection.append(buildValueFilters(state), buildRangeFilters(state), buildSortSection(state));
+
+  return controlsSection;
+};
+
+const buildToysPageContent = (state: IState) => {
+  const pageContainer = document.createElement('div');
+  const heading1 = document.createElement('h1');
+
+  const cardsElement = document.createElement('section');
+  const cardsTitle = document.createElement('h2');
+  const cardsList = document.createElement('ul');
+
+  pageContainer.classList.add('page-container');
+  heading1.classList.add('sr-only');
+  heading1.textContent = 'Выберите ёлочные игрушки';
+
+  cardsTitle.textContent = 'Карточки игрушек';
+  cardsTitle.classList.add('cards__title', 'sr-only');
+
+  cardsList.classList.add('cards__list');
+  cardsList.append(...buildCards(state));
+
+  cardsElement.classList.add('cards');
+  cardsElement.append(cardsTitle, cardsList);
+
+  pageContainer.append(heading1, buildToysPageControls(state), cardsElement);
+
+  return pageContainer;
+};
+
+export default buildToysPageContent;
