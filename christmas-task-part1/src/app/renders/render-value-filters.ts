41eb60df6acfa2674b53b7d8ee46850a47e409ab
Bonamente <56506035@protonmail.com>
2021-12-20 13:57:00 +0500

    refactor: move filters rendering into a separate function

---
+++ christmas-task-part1/src/app/renders/render-value-filters.ts
@@ -0,0 +1,10 @@
+import { IState } from '../app';
+import buildHtmlForFormElement from '../builders/controls/value-filters/form';
+
+const renderValueFilters = (state: IState): void =>{
+  const formElement = document.querySelector('.filters__form') as HTMLElement;
+  formElement.innerHTML = buildHtmlForFormElement(state);
+};
+
+
+export default renderValueFilters;
