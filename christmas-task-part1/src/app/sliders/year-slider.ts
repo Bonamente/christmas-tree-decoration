052b8408e0377791a30c114d6abf04f1a796d2f6
Bonamente <56506035@protonmail.com>
2021-12-20 13:35:53 +0500

    fix: sliders logic on settings reset

--- christmas-task-part1/src/app/sliders/year-slider.ts
+++ christmas-task-part1/src/app/sliders/year-slider.ts
@@ -1,21 +1,45 @@
 import noUiSlider, { target } from 'nouislider';
+import { IState } from '../app';
+import setLocalStorage from '../utils/set-local-storage';
+import renderCards from '../renders/render-cards';
 
-const yearSlider = <target>document.querySelector('.year__slider');
+const createYearSlider = (state: IState): void => {
+  const { yearFilter } = state.rangeFilters;
+  const { initMin, initMax, min, max } = yearFilter;
+  const yearSlider = <target>document.querySelector('.year__slider');
+  const outputMin = <HTMLElement>document.querySelector('.year__output--min');
+  const outputMax = <HTMLElement>document.querySelector('.year__output--max');
+  const outputs = [outputMin, outputMax];
 
-const outputMin = <HTMLElement>document.querySelector('.year__output--min');
-const outputMax = <HTMLElement>document.querySelector('.year__output--max');
-const outputs = [outputMin, outputMax];
+  noUiSlider.create(yearSlider, {
+    start: [min, max],
+    step: 10,
+    connect: true,
+    range: {
+      min: initMin,
+      max: initMax,
+    },
+  });
 
-noUiSlider.create(yearSlider, {
-  start: [1940, 2020],
-  step: 10,
-  connect: true,
-  range: {
-    min: 1940,
-    max: 2020,
-  },
-});
+  yearSlider?.noUiSlider?.on('update', (values, handle): void => {
+    outputs[handle].innerText = String(Math.round(Number(values[handle])));
 
-yearSlider?.noUiSlider?.on('update', function (values, handle): void {
-  outputs[handle].innerText = String(Math.round(Number(values[handle])));
-});
+    const [value1, value2] = values;
+    const curMinVal = Math.trunc(+value1);
+    const curMaxVal = Math.trunc(+value2);
+
+    state.rangeFilters.yearFilter.min = curMinVal;
+    state.rangeFilters.yearFilter.max = curMaxVal;
+
+    setLocalStorage(state);
+    renderCards(state);
+  });
+
+  const resetFiltersBtn = document.querySelector('.reset__filters-btn') as HTMLElement;
+
+  resetFiltersBtn.addEventListener('click', (): void => {
+    yearSlider?.noUiSlider?.set([initMin, initMax]);    
+  });
+};
+
+export default createYearSlider;
